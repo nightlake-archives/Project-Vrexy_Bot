c0e478c6de9656eda20f1a7dfc8492d51bb6492b
sadie brooklyn <senkodev@gmail.com>
2022-01-08 09:38:51 +0300

    fix(meta): get proper user tags

--- src/interactions/commands/meta.ts
+++ src/interactions/commands/meta.ts
@@ -6,41 +6,33 @@ module.exports = {
     async execute(bot: VrexyClient, interaction: CommandInteraction) {
         switch (interaction.options.getSubcommand()) {
             case 'about': {
-                function getUsernames(userIDs: Array<String>) {
-                    let usernames = []
-
-                    for (const userID in userIDs) {
-                        const user = bot.users.cache.get(userID)
-                        usernames.push(user.tag)
-                    }
-
-                    return usernames
-                }
+                const developers = (await Promise.all(['348591272476540928'].map(async id => (await bot.users.fetch(id)).tag))).join('\n');
+                const specials = (await Promise.all(['707675871355600967', '478823932913516544'].map(async id => (await bot.users.fetch(id)).tag))).join('\n');
 
                 interaction.reply({ embeds: [
                     {
-                        title: `:information_source: ${bot.fetchLangString('APIUD', 'META_ABOUT_TITLE', {'appName': 'Vrexy'})}`,
+                        title: `:information_source: ${bot.fetchLangString('en-US', 'META_ABOUT_TITLE', {'appName': 'Vrexy'})}`,
                         color: bot.color,
-                        description: bot.fetchLangString('APIUD', 'META_ABOUT_DESC', {'appName': 'Vrexy'}),
+                        description: bot.fetchLangString('en-US', 'META_ABOUT_DESC', {'appName': 'Vrexy'}),
                         fields: [
                             {
-                                name: `<:bot_dev:836605174696509471> ${bot.fetchLangString('APIUD', 'META_ABOUT_FIELDS_DEVELOPERS', {})}`,
-                                value: getUsernames(['348591272476540928']).join('\n')
+                                name: `<:bot_dev:836605174696509471> ${bot.fetchLangString('en-US', 'META_ABOUT_FIELDS_DEVELOPERS', {})}`,
+                                value: developers
                             },
                             {
-                                name: `:star: ${bot.fetchLangString('APIUD', 'META_ABOUT_FIELDS_SPECIALS', {})}`,
-                                value: getUsernames(['707675871355600967', '478823932913516544']).join('\n')
+                                name: `:star: ${bot.fetchLangString('en-US', 'META_ABOUT_FIELDS_SPECIALS', {})}`,
+                                value: specials
                             },
                             {
-                                name: `:link: ${bot.fetchLangString('APIUD', 'META_ABOUT_LINKS_TITLE', {})}`,
+                                name: `:link: ${bot.fetchLangString('en-US', 'META_ABOUT_LINKS_TITLE', {})}`,
                                 value: [
-                                    `:globe_americas: (${bot.fetchLangString('APIUD', 'META_ABOUT_LINKS_WEBSITE', {})})[https://vrexy.xyz]`
+                                    `:globe_americas: (${bot.fetchLangString('en-US', 'META_ABOUT_LINKS_WEBSITE', {})})[https://vrexy.xyz]`
                                 ].join('\n')
                             }
                         ]
                     }
                 ]})
             }
-        }
+        }       
     }
 }
