1f7bae3bc738095ada1be5c2ca4f8bb365630235
sadie brooklyn <senkodev@gmail.com>
2022-01-12 22:59:24 +0300

    chore(eslint): buildCommands

--- buildCommands.js
+++ buildCommands.js
@@ -1,24 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-require('dotenv').config()
+require('dotenv').config();
 
 const commands = [
 	new SlashCommandBuilder().setName('meta').setDescription('Info about Vrexy').addSubcommand(subcommand =>
-            subcommand.setName('about')
-                .setDescription('Views info about the bot')
-        )
+		subcommand.setName('about')
+			.setDescription('Views info about the bot')
+	),
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 if (process.env.GUILDID) {
-    rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), { body: commands })
-    .then(() => console.log('Successfully registered application commands for guild.'))
-    .catch(console.error);
-} else {   
-    rest.put(Routes.applicationCommands(process.env.CLIENTID), { body: commands })
-    .then(() => console.log('Successfully registered application commands for bot.'))
-    .catch(console.error);
+	rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), { body: commands })
+		.then(() => console.log('Successfully registered application commands for guild.'))
+		.catch(console.error);
+}
+else {
+	rest.put(Routes.applicationCommands(process.env.CLIENTID), { body: commands })
+		.then(() => console.log('Successfully registered application commands for bot.'))
+		.catch(console.error);
 }
