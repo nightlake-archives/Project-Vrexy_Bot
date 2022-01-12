cfef1a0ac6b72059e2f481f8c917be2602dae783
sadie brooklyn <senkodev@gmail.com>
2022-01-12 22:40:34 +0300

    chore: fix all fixable eslint problems

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -2,39 +2,39 @@ import { Client, Intents, Collection } from 'discord.js';
 import { readdirSync } from 'fs';
 
 export class VrexyClient extends Client {
-    slashInteractions: Collection<String, any>
-    componentInteractions: Collection<String, any>
-    langFiles: Record<string, any>
-    color: number
-    src: String
-    fetchLangString
-    
-    constructor() {
-        super({
-            intents: [Intents.FLAGS.GUILD_MEMBERS], allowedMentions: { parse: ['users'], repliedUser: false }
-        });
-        this.slashInteractions = new Collection();
-        this.componentInteractions = new Collection();
-        this.langFiles = new Collection();
-        this.color = 0xE67E22;
-        this.src = `${process.cwd()}/dist`
+	slashInteractions: Collection<string, any>;
+	componentInteractions: Collection<string, any>;
+	langFiles: Record<string, any>;
+	color: number;
+	src: string;
+	fetchLangString;
 
-        for (const langFile of readdirSync(`${process.cwd()}/i18n`).filter((file: String) => file.endsWith(".json"))) {
-            const langFileData = require(`${process.cwd()}/i18n/${langFile}`)
-            this.langFiles[langFile] = langFileData
-        }
+	constructor() {
+		super({
+			intents: [Intents.FLAGS.GUILD_MEMBERS], allowedMentions: { parse: ['users'], repliedUser: false },
+		});
+		this.slashInteractions = new Collection();
+		this.componentInteractions = new Collection();
+		this.langFiles = new Collection();
+		this.color = 0xE67E22;
+		this.src = `${process.cwd()}/dist`;
 
-        this.fetchLangString = (lang: string, translatebleString: string, variables: Record<string, any>): string => {
-            const langFile = this.langFiles[`${lang}.json`]
+		for (const langFile of readdirSync(`${process.cwd()}/i18n`).filter((file: string) => file.endsWith('.json'))) {
+			const langFileData = require(`${process.cwd()}/i18n/${langFile}`);
+			this.langFiles[langFile] = langFileData;
+		}
 
-            function formatVars(str: string, variables: Record<string, any>): string {
-                return str.replace(new RegExp("\{([^\{]+)\}", "g"), function(_unused, varName){
-                    return variables[varName];
-                })
-            }
+		this.fetchLangString = (lang: string, translatebleString: string, variables: Record<string, any>): string => {
+			const langFile = this.langFiles[`${lang}.json`];
 
-            return formatVars(langFile[translatebleString], variables);
-        }
-    }
+			function formatVars(str: string, variables: Record<string, any>): string {
+				return str.replace(new RegExp('\{([^\{]+)\}', 'g'), function(_unused, varName) {
+					return variables[varName];
+				});
+			}
+
+			return formatVars(langFile[translatebleString], variables);
+		};
+	}
 }
 
--- src/events/inter.ts
+++ src/events/inter.ts
@@ -1,20 +1,21 @@
 import { Interaction } from 'discord.js';
 
 module.exports = {
-    name: 'interactionCreate',
-    once: false,
-    async execute(interaction: any) {
-        if (interaction.isCommand()) {
-            const command = interaction.client.slashInteractions.get(interaction.commandName);
+	name: 'interactionCreate',
+	once: false,
+	async execute(interaction: any) {
+		if (interaction.isCommand()) {
+			const command = interaction.client.slashInteractions.get(interaction.commandName);
 
-            if (!command) return;
-        
-            try {   
-                await command.execute(interaction.client, interaction);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }        
-        }
-    }
-}
+			if (!command) return;
+
+			try {
+				await command.execute(interaction.client, interaction);
+			}
+			catch (error) {
+				console.error(error);
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
+	},
+};
