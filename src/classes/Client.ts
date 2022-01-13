9a557de14288edb303eb5c62e480d3e5b39d2ee2
sadie brooklyn <senkodev@gmail.com>
2022-01-13 09:41:10 +0300

    feat(client): remove old loaders

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -2,8 +2,6 @@ import { Client, Intents, Collection } from 'discord.js';
 import LocaleManager from './managers/LocaleManager';
 
 export class VrexyClient extends Client {
-	slashInteractions: Collection<string, any>;
-	componentInteractions: Collection<string, any>;
 	locale: LocaleManager;
 	color: number;
 	src: string;
@@ -12,8 +10,6 @@ export class VrexyClient extends Client {
 		super({
 			intents: [Intents.FLAGS.GUILD_MEMBERS], allowedMentions: { parse: ['users'], repliedUser: false },
 		});
-		this.slashInteractions = new Collection();
-		this.componentInteractions = new Collection();
 		this.locale = new LocaleManager();
 		this.color = 0xE67E22;
 		this.src = `${process.cwd()}/dist`;
--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const bot = new VrexyClient();
-
-const eventFiles = readdirSync(`${bot.src}/events`).filter((file: string) => file.endsWith('.js'));
-const slashFiles = readdirSync(`${bot.src}/interactions/commands`).filter((file: string) => file.endsWith('.js'));
-const commponentFiles = readdirSync(`${bot.src}/interactions/components`).filter(file => file.endsWith('.js'));
-
-for (const eventFile of eventFiles) {
-	const event = require(`${bot.src}/events/${eventFile.replace('.js', '')}`);
-	if (event.once) {
-		bot.once(event.name, (...args: Array<any>) => event.execute(...args));
-	}
-	else {
-		bot.on(event.name, (...args: Array<any>) => event.execute(...args));
-	}
-}
-
-for (const slashFile of slashFiles) {
-	const command = require(`${bot.src}/interactions/commands/${slashFile}`);
-	bot.slashInteractions.set(command.name, command);
-}
-
-for (const componentFile of commponentFiles) {
-	const component = require(`${bot.src}/interactions/components/${componentFile}`);
-	bot.componentInteractions.set(component.name, component);
-}
-
 bot.login(process.env.TOKEN);
