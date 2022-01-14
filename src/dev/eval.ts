4e238cea18d385568b1d1fca781f0c9a9226b52c
sadie brooklyn <senkodev@gmail.com>
2022-01-14 10:41:34 +0300

    feat(devUtils): add eval

---
+++ src/dev/eval.ts
@@ -0,0 +1,31 @@
+import { Message } from 'discord.js';
+import { VrexyClient } from 'src/classes/Client';
+import clean from 'src/utils/clean';
+
+export async function execute(bot: VrexyClient, message: Message, args: string[]): Promise<Message> {
+	if (args.includes('bot.token')) return message.reply('<:app_error:894254521025445979> No!');
+	let codex = args.join(' ').replaceAll('```js', '').replaceAll('```ts', '');
+
+	if (codex.indexOf('```', codex.length - 4) !== -1) {
+		codex = codex.slice(0, -3);
+	}
+
+	const code = codex.replace('bot.token', 'no');
+	try {
+		const evaled = eval(code);
+		message.react('✅');
+		return message.reply({ embeds: [{
+			title: 'Done!',
+			description: `\`\`\`js\n${await clean(bot, evaled)}}\n\`\`\``,
+			color: bot.color,
+		}] });
+	}
+	catch (error) {
+		message.react('❌');
+		return message.reply({ embeds: [{
+			title: 'Error!',
+			description: `\`\`\`js\n${await clean(bot, error)}}\n\`\`\``,
+			color: bot.color,
+		}] });
+	}
+}
---
+++ src/utils/clean.ts
@@ -0,0 +1,7 @@
+import { VrexyClient } from 'src/classes/Client';
+import { inspect } from 'util';
+
+export default async function clean(bot: VrexyClient, text: unknown) {
+	const inspectedText = inspect(await text, { depth: 1 });
+	return inspectedText.replaceAll(bot.token, 'No!');
+}
