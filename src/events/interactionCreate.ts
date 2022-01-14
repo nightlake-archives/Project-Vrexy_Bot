0bbe990a4b91f70a7efb02aebcc18557714b8fa0
sadie brooklyn <senkodev@gmail.com>
2022-01-14 09:32:42 +0300

    feat(components): add `interactionCreate` handler

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -14,4 +14,17 @@ export async function execute(bot: VrexyClient, interaction: (CommandInteraction
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	}
+	else if (interaction.isMessageComponent) {
+		const interactionData = interaction.customId.split(':');
+		const component = bot.components.get(interactionData[0]);
+		if (!component) return;
+
+		try {
+			await component.execute(bot, interaction, interactionData);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an issue while executing this component!', ephemeral: true });
+		}
+	}
 }
--- src/types/Component.type.ts
+++ src/types/Component.type.ts
@@ -2,5 +2,5 @@ import { ButtonInteraction, SelectMenuInteraction } from 'discord.js';
 import { VrexyClient } from 'src/classes/Client';
 
 export interface Component {
-	execute: (bot: VrexyClient, interaction: ButtonInteraction | SelectMenuInteraction) => void;
+	execute: (bot: VrexyClient, interaction: ButtonInteraction | SelectMenuInteraction, interactionData: string[]) => void;
 }
