1ffef45379b8812a9074ff1b932c1a9065e3a037
sadie brooklyn <senkodev@gmail.com>
2022-01-14 11:18:11 +0300

    feat(messageCreate): bot.user.toString() wont work

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,20 +3,22 @@ import { VrexyClient } from 'src/classes/Client';
 
 export async function execute(bot: VrexyClient, message: Message) {
 	if (message.author.bot) return;
-	if (message.content.startsWith(bot.user.toString())) {
+
+	if (message.content.startsWith(`<@!${bot.user.id}>`)) {
 		if (!bot.devs.includes(message.author.id)) return;
 
-		const args = message.content.slice(bot.user.toString().length).trim().split(/ +/);
+		const args = message.content.slice(`<@!${bot.user.id}>`.length).trim().split(/ +/);
 		const commandName = args.shift().toLowerCase();
+		console.log(args, commandName);
 		const command = bot.devUtils.get(commandName);
 
 		if (!command) return;
 
 		try {
-			command.execute(bot, message, args);
+			return command.execute(bot, message, args);
 		}
 		catch (error) {
-			await message.reply({ embeds: [{
+			return await message.reply({ embeds: [{
 				title: '<:app_error:894254521025445979>	You fucked up!',
 				description: error.toString(),
 			}] });
