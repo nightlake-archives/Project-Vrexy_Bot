7076ca1c720c8d25d4a51266485612789ce68e92
sadie brooklyn <senkodev@gmail.com>
2022-01-15 09:18:14 +0300

    feat(db): add mongodb integration

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -1,4 +1,5 @@
 import { Client, Intents, Collection } from 'discord.js';
+import { Db, MongoClient } from 'mongodb';
 import { Command } from 'src/types/Command.type';
 import { Component } from 'src/types/Component.type';
 import { DevUtil } from 'src/types/devCommand.type';
@@ -9,10 +10,16 @@ import EventManager from './managers/EventManager';
 import LocaleManager from './managers/LocaleManager';
 
 export class VrexyClient extends Client {
+	// managers and collections
 	locale: LocaleManager;
 	commands: Collection<string, Command>;
 	components: Collection<string, Component>;
 	devUtils: Collection<string, DevUtil>;
+	// mongo connection
+	mongo: MongoClient;
+	connection: Promise<MongoClient>;
+	db: Db;
+	// bot info
 	color: number;
 	devs: string[];
 
@@ -24,6 +31,7 @@ export class VrexyClient extends Client {
 		});
 
 		this.locale = new LocaleManager();
+		this.mongo = new MongoClient('mongodb://localhost:27017');
 		this.color = 0xE67E22;
 		this.devs = ['348591272476540928'];
 		this.token = token;
@@ -34,6 +42,8 @@ export class VrexyClient extends Client {
 		this.commands = new CommandManager().load();
 		this.components = new ComponentManager().load();
 		this.devUtils = new DevUtilManager().load();
+		this.connection = this.mongo.connect();
+		this.db = this.mongo.db('vrexy');
 		this.login(this.token);
 
 		process.on('unhandledRejection', error => {
