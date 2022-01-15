b98d975a524140b89dbb1592ead09fa8fd4358e1
sadie brooklyn <senkodev@gmail.com>
2022-01-15 19:08:28 +0300

    feat(roleDrop): prepare for groups

--- src/commands/roledrop.ts
+++ src/commands/roledrop.ts
@@ -2,35 +2,37 @@ import { VrexyClient } from '../classes/Client';
 import { CommandInteraction } from 'discord.js';
 
 export async function execute(bot: VrexyClient, interaction: CommandInteraction) {
+	switch (interaction.options.getSubcommandGroup()) {
+	default: {
+		switch (interaction.options.getSubcommand()) {
+		case 'deploy': {
+			const collection = bot.db.collection('guild');
+			const guildData = await collection.findOne({ guild: interaction.guild.id });
 
-	switch (interaction.options.getSubcommand()) {
-	case 'deploy': {
-
-		const collection = bot.db.collection('guild');
-		const guildData = await collection.findOne({ guild: interaction.guild.id });
-
-		const categoryRoles = [{
-			label: 'the rock',
-			description: 'fuck',
-			value: 'theROck',
-		},
-		{
-			label: 'th',
-			description: 'your',
-			value: 'mom',
-		}];
+			const categoryRoles = [{
+				label: 'the rock',
+				description: 'fuck',
+				value: 'theROck',
+			},
+			{
+				label: 'th',
+				description: 'your',
+				value: 'mom',
+			}];
 
 
-		interaction.reply({
-			content: guildData._id.toString(), components: [{
-				type: 'ACTION_ROW',
-				components: [{
-					type: 'SELECT_MENU',
-					customId: 'roleDropdown:RoleDropdownSelectMenu',
-					options: categoryRoles,
+			interaction.reply({
+				content: guildData._id.toString(), components: [{
+					type: 'ACTION_ROW',
+					components: [{
+						type: 'SELECT_MENU',
+						customId: 'roleDropdown:RoleDropdownSelectMenu',
+						options: categoryRoles,
+					}],
 				}],
-			}],
-		});
+			});
+		}
+		}
 		break;
 	}
 	}
