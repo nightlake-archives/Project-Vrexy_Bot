1a8ca92dc5ce0fff8dd4f1d10c93a1ba624c9c54
sadie brooklyn <senkodev@gmail.com>
2022-01-18 20:56:44 +0300

    feat: add autocomplete support

--- src/types/Command.type.ts
+++ src/types/Command.type.ts
@@ -1,14 +1,15 @@
-import { BaseCommandInteraction } from 'discord.js';
+import { AutocompleteInteraction, BaseCommandInteraction } from 'discord.js';
 import { VrexyClient } from 'src/classes/Client';
 import { SlashCommandBuilder, ContextMenuCommandBuilder } from '@discordjs/builders';
 
 interface CommandData {
-	slash?: SlashCommandBuilder,
-	context?: ContextMenuCommandBuilder
+	slash?: SlashCommandBuilder;
+	context?: ContextMenuCommandBuilder;
 }
 
 export interface Command {
 	data: CommandData;
 	run: (bot: VrexyClient, interaction: BaseCommandInteraction) => void;
+	autocomplete: (bot: VrexyClient, interaction: AutocompleteInteraction) => void;
 	custom?: Record<string, unknown>;
 }
