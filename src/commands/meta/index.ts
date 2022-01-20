f8eb38b733d48e78f60d65b3cbffd0f359953d5c
sadie brooklyn <senkodev@gmail.com>
2022-01-20 23:22:16 +0300

    feat(meta): add ping command

--- src/commands/meta/index.ts
+++ src/commands/meta/index.ts
@@ -4,5 +4,7 @@ export const data = {
 	slash: new SlashCommandBuilder()
 		.setName('meta')
 		.setDescription('VrexyGroupCommands.Meta')
-		.addSubcommand(command => command.setName('about').setDescription('Views info about the bot')),
+		.addSubcommand(command => command.setName('about').setDescription('Views info about the bot.'))
+		.addSubcommand(command => command.setName('changelog').setDescription('Views what has changed in the bot.'))
+		.addSubcommand(command => command.setName('ping').setDescription('Gets the bot\'s response time.')),
 };
---
+++ src/commands/meta/ping.ts
@@ -0,0 +1,7 @@
+import { CommandInteraction } from 'discord.js';
+import i18next from 'i18next';
+import { VrexyClient } from 'src/classes/Client';
+
+export async function run(bot: VrexyClient, interaction: CommandInteraction) {
+	interaction.reply(i18next.t('meta:ping', { ping: bot.ws.ping, lng: interaction.locale }));
+}
